7a18bae393fefebdbcaa9e29fc3b70289c3eb142
Caleb Knox <knoxcaleb@aol.com>
2016-09-10 20:31:16 +0000

    finished second_try

--- lib/student.rb
+++ lib/student.rb
@@ -1,1 +1,83 @@
+class Student
+  attr_accessor :id, :name, :grade
 
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students;
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade TEXT
+      );
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) VALUES (?, ?);
+    SQL
+
+    DB[:conn].execute(sql, name, grade)
+  end
+
+  def self.new_from_db(row)
+    self.new.tap do |student|
+      student.id = row[0]
+      student.name = row[1]
+      student.grade = row[2]
+    end
+  end
+
+    def self.find_by_name(name)
+      sql = <<-SQL
+        SELECT * FROM students WHERE name = ? LIMIT 1;
+      SQL
+
+      DB[:conn].execute(sql, name).collect do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
+    def self.count_all_students_in_grade_9
+      sql = <<-SQL
+        SELECT * FROM students WHERE grade = 9;
+      SQL
+
+      DB[:conn].execute(sql)
+    end
+
+    def self.students_below_12th_grade
+      sql = <<-SQL
+        SELECT * FROM students WHERE grade < 12;
+      SQL
+
+      DB[:conn].execute(sql)
+    end
+
+    def self.all
+      sql = <<-SQL
+        SELECT * FROM students;
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
+    end
+
+    def self.first_student_in_grade_10
+      sql = <<-SQL
+        SELECT * FROM students WHERE grade = 10 ORDER BY id ASC LIMIT 1;
+      SQL
+
+      DB[:conn].execute(sql).collect {|row| self.new_from_db(row)}.first
+    end
+end
